150e8da3a264c825e9ef59ff2549e84f656755fc
Leonardohenrik <leonardoface_519@hotmail.com>
2025-10-02 13:48:26 +0000

    commit

--- Estudo_Casa/Repetição/Exercicio_09/main.c
+++ Estudo_Casa/Repetição/Exercicio_09/main.c
@@ -2,31 +2,34 @@
 
 int main(){
     int num;
-    int resultado = 0, numeros_validos = 0;
+    int soma = 0, quantidade = 0;
     float media;
+    while(1){
+        do{
+            printf("Digite um número entre 1 e 100 ou 0 para sair: ");
+            scanf("%d", &num);
 
-    do{
-        printf("Digite números inteiros que você deseja testar ou 0 para sair: ");
-        scanf("%d", &num);
+            if(num < 0 || num > 100){
+
+                printf("Número inválido digite novamente\n");
+            }
+
+        }while(num < 0 || num >100);
         if(num == 0){
-            printf("Saindo do programa!\n");
-
-        }else if(num < 1 || num > 100){
-            printf("Número inválido digite novamente: \n");
-        }else{
-            resultado = resultado + num;
-            numeros_validos = numeros_validos + 1;
+            printf("Você escolheu sair até mais!\n");
+            break;
         }
-
-    }while(num != 0);
-    if(numeros_validos > 0){
-        media = resultado / numeros_validos;
-        printf("A média dos números válidos digitados de 1 a 100 é de: %.2f\n", media);
+        quantidade = quantidade + 1;
+        soma = soma + num;
+    }
+    if(quantidade > 0){
+        media = (float)soma / quantidade;
+        printf("A média dos números válidos digitados é de %.2f\n", media);
 
     }else{
-        printf("Nenhum números válido foi digitado\n");
+        printf("Nenhum número válido foi digitado!\n");
     }
-
+    
     
     
     return 0;
---
+++ Estudo_Casa/Repetição/Exercicio_10/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int collatz(int n){
+    if(n %2 == 0){
+        return 
+    }
+}
+
+int main(){
+    
+    
+    
+    
+    
+    
+    return 0;
+}
